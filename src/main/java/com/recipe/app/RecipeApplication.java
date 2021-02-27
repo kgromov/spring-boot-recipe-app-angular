a6b46fe83d2a83e3eaddd6b943c71a8ac275a52d
Konstantin <konstantin@revimedia.com>
2021-02-27 17:45:50 +0200

    Integrate actuator; add git info

--- src/main/java/com/recipe/app/RecipeApplication.java
+++ src/main/java/com/recipe/app/RecipeApplication.java
@@ -3,8 +3,6 @@ package com.recipe.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.metrics.jfr.FlightRecorderApplicationStartup;
 
 @SpringBootApplication
 public class RecipeApplication {
@@ -12,7 +10,7 @@ public class RecipeApplication {
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(RecipeApplication.class);
 		// for some reason returns 405 (even if enable endpoint explicitly)
-//		application.setApplicationStartup(new BufferingApplicationStartup(30_000));
+		application.setApplicationStartup(new BufferingApplicationStartup(4096));
 		// requires additional parameters - -XX:+UnlockCommercialFeatures -XX:+FlightRecorder
 		// but still does not work - NoClassDefFound (seems it's time to switch to java 11)
 //		application.setApplicationStartup(new FlightRecorderApplicationStartup());
